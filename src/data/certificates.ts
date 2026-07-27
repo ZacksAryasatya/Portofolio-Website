ef692a9045015ad962f61d7bf7b72f3158d0c25c
Zaky <syafazaqi02@gmail.com>
2026-07-27 15:08:06 +0700

    ...

--- src/data/certificates.ts
+++ src/data/certificates.ts
@@ -1,9 +1,9 @@
-import cert1 from '@/assets/images/sertifikat/Belajar AI dicoding1.jpg'
-import cert2 from '@/assets/images/sertifikat/Belajar AI dicoding2.png'
-import cert3 from '@/assets/images/sertifikat/WIA.jpg'
-import cert4 from '@/assets/images/sertifikat/dicoding pemograman.jpg'
-import cert5 from '@/assets/images/sertifikat/dinacom.jpg'
-import cert6 from '@/assets/images/sertifikat/timedoor.jpg'
+import cert1 from '@/assets/images/Sertifikat/Belajar AI dicoding1.jpg'
+import cert2 from '@/assets/images/Sertifikat/Belajar AI dicoding2.png'
+import cert3 from '@/assets/images/Sertifikat/WIA.jpg'
+import cert4 from '@/assets/images/Sertifikat/dicoding pemograman.jpg'
+import cert5 from '@/assets/images/Sertifikat/dinacom.jpg'
+import cert6 from '@/assets/images/Sertifikat/timedoor.jpg'
 
 export interface Certificate {
   id: string
